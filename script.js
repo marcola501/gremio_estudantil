3ff0146e80d4981a36d75b277dbf66dbf8d84196
Marcos Augusto <marcos.augusto04demaio2009@gmail.com>
2026-03-24 11:34:14 -0300

    Estrutura do site
    Estrutura inicial, sem os posts em formato de article

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
                     
 function alterar_tema(){
     const body = document.body;
-    const paragrafo = document.querySelector("p");
-    const titulo = document.querySelector("h1");
+    const paragrafo = /** @type {NodeListOf<HTMLElement>} */ document.querySelectorAll("p");
+    const titulo =/** @type {NodeListOf<HTMLElement>} */  document.querySelectorAll("h1");
     const titulo2 = /** @type {NodeListOf<HTMLElement>} */ (document.querySelectorAll("h2, h3, h4, h5, h6"));
     const temaEscuro = ["#2b2424", "#f9dfdf", "#f5d9d9", "#f1dada"];
     const temaClaro = ["#f0f0f0", "#333", "#444", "#555"];
@@ -12,15 +12,23 @@ function alterar_tema(){
     body.style.transition = "background-color 0.5s ease";
     if (corAtual === "rgb(240, 240, 240)"){
         body.style.backgroundColor = temaEscuro[0];
-        paragrafo.style.color = temaEscuro[1];
-        titulo.style.color = temaEscuro[2];
+        for (let i = 0;i<paragrafo.length;i++){
+             paragrafo[i].style.color = temaEscuro[1];
+        }
+        for (let i = 0; i < titulo.length; i++) {
+            titulo[i].style.color = temaEscuro[2];
+        }
         for (let i = 0; i < titulo2.length; i++) {
             titulo2[i].style.color = temaEscuro[3];
         }
     } else {
         body.style.backgroundColor = temaClaro[0];
-        paragrafo.style.color = temaClaro[1];
-        titulo.style.color = temaClaro[2];
+        for (let i = 0; i < paragrafo.length; i++) {
+            paragrafo[i].style.color = temaClaro[1];
+        }
+        for (let i = 0; i < titulo.length; i++) {
+            titulo[i].style.color = temaClaro[2];
+        }
         for (let i = 0; i < titulo2.length; i++) {
             titulo2[i].style.color = temaClaro[3];
         }
